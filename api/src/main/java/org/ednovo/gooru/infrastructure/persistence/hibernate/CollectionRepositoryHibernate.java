adaf8bd13447bca6609aa0e8d0e781ed21b126e0
Sheeban <sheeban.s@goorulearning.org>
2014-02-14 20:37:43 +0530

    added fix for collection create include

--- api/src/main/java/org/ednovo/gooru/infrastructure/persistence/hibernate/CollectionRepositoryHibernate.java
+++ api/src/main/java/org/ednovo/gooru/infrastructure/persistence/hibernate/CollectionRepositoryHibernate.java
@@ -756,10 +756,8 @@ public class CollectionRepositoryHibernate extends BaseRepositoryHibernate imple
 
 	@Override
 	public String getParentCollection(String collectionGooruOid, String gooruUid) {
-		String hql = "select cc.gooru_oid  as gooruOid  from collection_item ci inner join resource r on r.content_id = ci.resource_content_id inner join content cr on cr.content_id = r.content_id inner join content cc on cc.content_id = ci.collection_content_id inner join collection co on  co.content_id = ci.collection_content_id  where cr.gooru_oid=:gooruOid and cc.user_uid =:gooruUid and co.collection_type = 'folder'";
+		String hql = "select cc.gooru_oid  as gooruOid  from collection_item ci inner join resource r on r.content_id = ci.resource_content_id inner join content cr on cr.content_id = r.content_id inner join content cc on cc.content_id = ci.collection_content_id inner join collection co on  co.content_id = ci.collection_content_id  where cr.gooru_oid='"+collectionGooruOid+"' and cc.user_uid ='"+gooruUid+"'and co.collection_type = 'folder'";
 		Query query = getSession().createQuery(hql);
-		query.setParameter("gooruOid", collectionGooruOid);
-		query.setParameter("gooruUid", gooruUid);
 		return query.list() != null ? (String)query.list().get(0) : null;
 	}
 }
