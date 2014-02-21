a609a7b07ac53009e27f8b1d03cd2d6097d2bace
charles.k <charles.kurusev@goorulearning.org>
2014-02-21 17:43:45 +0530

    Added fix for new standard api

--- rest-v2-app/src/main/java/org/ednovo/gooru/controllers/v2/api/CollectionRestV2Controller.java
+++ rest-v2-app/src/main/java/org/ednovo/gooru/controllers/v2/api/CollectionRestV2Controller.java
@@ -416,7 +416,7 @@ public class CollectionRestV2Controller extends BaseController implements Consta
 	}
 	
 	@RequestMapping(value = { "/standards" }, method = RequestMethod.GET)
-	public ModelAndView getCollectionStandards(HttpServletRequest request, @RequestParam(value = ID) Integer codeId,@RequestParam(value="query",required =false) String query, HttpServletResponse resHttpServletResponse
+	public ModelAndView getCollectionStandards(HttpServletRequest request, @RequestParam(value = ID,required = false) Integer codeId,@RequestParam(value="query",required =false) String query, HttpServletResponse resHttpServletResponse
 			,@RequestParam(value = OFFSET_FIELD, required = false, defaultValue = "0") Integer offset, @RequestParam(value = LIMIT_FIELD, required = false, defaultValue = "10") Integer limit,@RequestParam(value = SKIP_PAGINATION, required = false, defaultValue = FALSE) boolean skipPagination) throws Exception {
 		User user = (User) request.getAttribute(Constants.USER);
 		return toModelAndViewWithIoFilter(this.getCollectionService().getCollectionStandards(codeId,query,limit,offset,skipPagination), RESPONSE_FORMAT_JSON, EXCLUDE_ALL, true, COLLECTION_STANDARDS_INCLUDES);
