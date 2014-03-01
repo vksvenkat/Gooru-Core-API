0a5a0cafd56171a492469c7aeb1aba7c02348657
Sheeban <sheeban.s@goorulearning.org>
2014-03-01 14:12:33 +0530

    included profile image

--- api/src/main/java/org/ednovo/gooru/domain/service/CollaboratorServiceImpl.java
+++ api/src/main/java/org/ednovo/gooru/domain/service/CollaboratorServiceImpl.java
@@ -70,6 +70,8 @@ public class CollaboratorServiceImpl extends BaseServiceImpl implements Collabor
 	@Autowired
 	private CollaboratorRepository collaboratorRepository;
 
+	@Autowired
+	private UserManagementService userManagementService;
 
 	@Autowired
 	private ContentService contentService;
