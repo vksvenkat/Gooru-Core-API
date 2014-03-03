957c8f12a0ad5097d9b6c8e4f1e2b1d768444bc0
cyril <cyrilraja.m@bambeeq.com>
2014-03-03 12:59:58 +0530

    *Implemented CORE-439

--- api/src/main/java/org/ednovo/gooru/domain/service/userManagement/UserManagementServiceImpl.java
+++ api/src/main/java/org/ednovo/gooru/domain/service/userManagement/UserManagementServiceImpl.java
@@ -455,7 +455,7 @@ public class UserManagementServiceImpl extends BaseServiceImpl implements UserMa
 			}
 		}
 		
-		if (user != null && sendConfirmationMail && inviteuser.size() < 0) {
+		if (user != null && sendConfirmationMail && inviteuser.size() <= 0) {
 			if (isAdminCreateUser) {
 				this.getMailHandler().sendMailToConfirm(user.getGooruUId(), password, accountType, userToken.getToken(), null, gooruBaseUrl, mailConfirmationUrl, null, null);
 			} else {
