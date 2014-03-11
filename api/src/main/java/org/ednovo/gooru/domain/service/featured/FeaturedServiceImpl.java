376aa5aba793ab05fcc3b32725e568eee8b0806b
charles.k <charles.kurusev@goorulearning.org>
2014-03-11 21:38:05 +0530

    Added copyrights and Modified existing copyrights template

--- api/src/main/java/org/ednovo/gooru/domain/service/featured/FeaturedServiceImpl.java
+++ api/src/main/java/org/ednovo/gooru/domain/service/featured/FeaturedServiceImpl.java
@@ -1,26 +1,32 @@
-/////////////////////////////////////////////////////////////
-// FeaturedServiceImpl.java
-// gooru-api
-// Created by Gooru on 2014
-// Copyright (c) 2014 Gooru. All rights reserved.
-// http://www.goorulearning.org/
-// Permission is hereby granted, free of charge, to any person obtaining
-// a copy of this software and associated documentation files (the
-// "Software"), to deal in the Software without restriction, including
-// without limitation the rights to use, copy, modify, merge, publish,
-// distribute, sublicense, and/or sell copies of the Software, and to
-// permit persons to whom the Software is furnished to do so, subject to
-// the following conditions:
-// The above copyright notice and this permission notice shall be
-// included in all copies or substantial portions of the Software.
-// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
-// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
-// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
-// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
-// LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
-// OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
-// WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-/////////////////////////////////////////////////////////////
+/*
+*FeaturedServiceImpl.java
+* gooru-api
+* Created by Gooru on 2014
+* Copyright (c) 2014 Gooru. All rights reserved.
+* http://www.goorulearning.org/
+*      
+* Permission is hereby granted, free of charge, to any 
+* person obtaining a copy of this software and associated 
+* documentation. Any one can use this software without any 
+* restriction and can use without any limitation rights 
+* like copy,modify,merge,publish,distribute,sub-license or 
+* sell copies of the software.
+* The seller can sell based on the following conditions:
+* 
+* The above copyright notice and this permission notice shall be   
+* included in all copies or substantial portions of the Software. 
+*
+*  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY    
+*  KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE  
+*  WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR   
+*  PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS 
+*  OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR 
+*  OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT 
+*  OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION 
+*  WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN 
+*  THE SOFTWARE.
+*/
+
 package org.ednovo.gooru.domain.service.featured;
 
 import java.util.ArrayList;
@@ -720,7 +726,7 @@ public class FeaturedServiceImpl implements FeaturedService, ParameterProperties
 				User lastUpdatedUser = this.getUserRepository().findUserByPartyUid(String.valueOf(object[5]));
 				Collection featuredCollection = this.getCollectionService().getCollection(String.valueOf(object[0]), true, true, false, user, "commentCount");
 				Long comment = this.getCommentRepository().getCommentCount(String.valueOf(object[0]), null, "notdeleted");
-				Long collectionItem = this.getCollectionRepository().getCollectionItemCount(String.valueOf(object[0]), "private,public,anyonewithlink", null);
+				Long collectionItem = this.getCollectionRepository().getCollectionItemCount(String.valueOf(object[0]), "private,public,anyonewithlink",null);
 				Iterator<Code> iter = featuredCollection.getTaxonomySet().iterator();
 				Map<Integer, List<Map<String, Object>>> codeParentsMap = new HashMap<Integer, List<Map<String, Object>>>();
 				while (iter.hasNext()) {
