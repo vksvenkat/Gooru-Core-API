2c0e9b01a1aabdd053b72aa37eb6de845cc988b3
cyril <cyrilraja.m@bambeeq.com>
2014-03-13 16:37:38 +0530

    *Fixed meta info
    Conflicts:
    	gooru-
    core/src/main/java/org/ednovo/gooru/core/api/model/UserTransModel.java

--- gooru-core/src/main/java/org/ednovo/gooru/core/api/model/UserTransModel.java
+++ gooru-core/src/main/java/org/ednovo/gooru/core/api/model/UserTransModel.java
@@ -1,3 +1,32 @@
+/*******************************************************************************
+ * UserTransModel.java
+ *  gooru-core
+ *  Created by Gooru on 2014
+ *  Copyright (c) 2014 Gooru. All rights reserved.
+ *  http://www.goorulearning.org/
+ *       
+ *  Permission is hereby granted, free of charge, to any 
+ *  person obtaining a copy of this software and associated 
+ *  documentation. Any one can use this software without any 
+ *  restriction and can use without any limitation rights 
+ *  like copy,modify,merge,publish,distribute,sub-license or 
+ *  sell copies of the software.
+ *  
+ *  The seller can sell based on the following conditions:
+ *  
+ *  The above copyright notice and this permission notice shall be   
+ *  included in all copies or substantial portions of the Software. 
+ * 
+ *   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY    
+ *   KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE  
+ *   WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR   
+ *   PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE       AUTHORS 
+ *   OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR 
+ *   OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT 
+ *   OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION 
+ *   WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN 
+ *   THE SOFTWARE.
+ ******************************************************************************/
 package org.ednovo.gooru.core.api.model;
 
 import java.util.Map;
@@ -12,7 +41,6 @@ import org.ednovo.gooru.core.api.model.PartyCustomField;
 import org.ednovo.gooru.core.api.model.User;
 import org.ednovo.gooru.core.api.model.UserRole;
 import org.ednovo.gooru.core.api.model.UserRoleAssoc;
-
 
 import flexjson.JSON;
 
@@ -45,7 +73,7 @@ public class UserTransModel extends OrganizationModel implements IndexableEntry 
 	private String accountCreatedType;
 	private Boolean isDeleted;
 	private Set<PartyCustomField> customFields;
-	private Map<String, Map<String, String>> meta;
+	private Map<String, Map<String, Object>> meta;
 	private String organizationName;
 
 	@Override
@@ -303,11 +331,11 @@ public class UserTransModel extends OrganizationModel implements IndexableEntry 
 		return customFields;
 	}
 
-	public void setMeta(Map<String, Map<String, String>> meta) {
-		this.meta = meta;
+	public void setMeta(Map<String, Map<String, Object>> map) {
+		this.meta = map;
 	}
 
-	public Map<String, Map<String, String>> getMeta() {
+	public Map<String, Map<String, Object>> getMeta() {
 		return meta;
 	}
 
@@ -318,6 +346,5 @@ public class UserTransModel extends OrganizationModel implements IndexableEntry 
 	public String getOrganizationName() {
 		return organizationName;
 	}
-
 
 }
