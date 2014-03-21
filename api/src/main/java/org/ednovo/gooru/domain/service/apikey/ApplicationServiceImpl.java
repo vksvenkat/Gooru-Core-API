beb4e6842567d825931ec17daac3a047e8d4f5cb
purnima <purnima@goorulearning.org>
2014-03-21 13:32:33 +0530

    Core -589 Add super admin entity operations to map an organization with APIKey for admin project

--- api/src/main/java/org/ednovo/gooru/domain/service/apikey/ApplicationServiceImpl.java
+++ api/src/main/java/org/ednovo/gooru/domain/service/apikey/ApplicationServiceImpl.java
@@ -72,7 +72,7 @@ public class ApplicationServiceImpl extends BaseServiceImpl implements Applicati
 	}
 
 	@Override
-	public ActionResponseDTO<ApiKey> saveApplication(ApiKey apikey, User user) throws Exception{
+	public ActionResponseDTO<ApiKey> saveApplication(ApiKey apikey, User user ,String organizationUid) throws Exception{
 		Errors error = validateApiKey(apikey);
 		if (!error.hasErrors()) {
 			PartyCustomField partyCustomField = partyService.getPartyCustomeField(user.getPartyUid(), ConstantProperties.ORG_ADMIN_KEY, user);
@@ -81,10 +81,20 @@ public class ApplicationServiceImpl extends BaseServiceImpl implements Applicati
 				if(organization == null){
 					throw new RuntimeException("Organization not found !");
 				}
+				 //If organization is passed from superadmin use it else set loggedin users organization details
+                if(organizationUid != null){
+                        organization = null;
+                        organization = organizationService.getOrganizationById(organizationUid);
+                        apikey.setOrganization(organization);
+                }
+                else{
+                        apikey.setOrganization(organization);
+                }
+
 				apikey.setActiveFlag(1);
 				apikey.setSecretKey(UUID.randomUUID().toString());
 				apikey.setKey(UUID.randomUUID().toString());
-				apikey.setOrganization(organization);
+				//apikey.setOrganization(organization);
 				apikey.setLimit(-1);
 				apikey.setDescription(apikey.getDescription());
 				CustomTableValue type = this.getCustomTableRepository().getCustomTableValue(CustomProperties.Table.APPLICATION_STATUS.getTable(), CustomProperties.ApplicationStatus.DEVELOPMENT.getApplicationStatus());
