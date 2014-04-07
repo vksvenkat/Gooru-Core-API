3bd2c86251cb2216ffb125397be0e6878f3e145e
charles.k <charles.kurusev@goorulearning.org>
2014-04-07 17:05:15 +0530

    Updated Text in Gooru Class Invite Email

--- api/src/main/java/org/ednovo/gooru/infrastructure/mail/MailHandler.java
+++ api/src/main/java/org/ednovo/gooru/infrastructure/mail/MailHandler.java
@@ -896,7 +896,7 @@ public class MailHandler extends ServerValidationUtils implements ConstantProper
 		}
 	}
 	
-	public void sendMailToInviteUser(String email, String gooruOid, User user, String title, String gender, String noun,String inviteFrom, String inviteUser) {
+	public void sendMailToInviteUser(String email, String gooruOid, User user, String title, String inviteFrom, String inviteUser) {
 		
 		final String serverpath = this.getServerConstants().getProperty(SERVERPATH);
 			EventMapping eventMapping = this.getEventService().getTemplatesByEventName(CustomProperties.EventMapping.SEND_MAIL_TO_INVITE_USER_CLASS.getEvent());
@@ -906,11 +906,9 @@ public class MailHandler extends ServerValidationUtils implements ConstantProper
 			map.put(TEACHERNAME ,user.getUsername());
 			map.put(MEMBERMAILID, email);
 			map.put(GOORU_OID, gooruOid);
-			map.put(GENDER, gender);
-			map.put(NOUN, noun);
 			map.put(RECIPIENT, email);
 			map.put(HTMLCONTENT, generateMessage((String) map.get(HTMLCONTENT), map));
-			map.put(SUBJECT, "You’re invited to join "+ gender + user.getUsername()+"’s" + " class \""+title+"\"");
+			map.put(SUBJECT, "You’re invited to the Gooru Class \""+title+"\"");
 			map.put(CONTENT, generateMessage((String) map.get(TEXTCONTENT), map));
 			map.put("from", inviteFrom);
 			map.put(BCC, getConfigSetting(ConfigConstants.MAIL_BCC_SUPPORT, TaxonomyUtil.GOORU_ORG_UID));
