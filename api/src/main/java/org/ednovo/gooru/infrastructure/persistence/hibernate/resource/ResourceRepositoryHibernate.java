947d26f5dd418bbfa0a298b7c5d8a4e34f2bc7e9
Rishwanth <rishwanth.avr@goorulearning.org>
2014-04-22 13:22:55 +0530

    CORE-738 - getResourceSourceAttribution() added

--- api/src/main/java/org/ednovo/gooru/infrastructure/persistence/hibernate/resource/ResourceRepositoryHibernate.java
+++ api/src/main/java/org/ednovo/gooru/infrastructure/persistence/hibernate/resource/ResourceRepositoryHibernate.java
@@ -978,4 +978,17 @@ public class ResourceRepositoryHibernate extends BaseRepositoryHibernate impleme
 		return ltiContentAssoc.size() == 0 ? null : ltiContentAssoc.get(0).getResource();
 	}
 
+	 @Override
+	 public List<String> getResourceSourceAttribution() {
+            String sql = "select attribution from resource_source where  resource_source_id in ( ";
+            sql += " select r.resource_source_id from resource r inner join resource_source rs ";
+            sql += " on rs.resource_source_id = r.resource_source_id group by r.resource_source_id order by count(content_id) desc ) ";
+            Session session = getSessionFactory().getCurrentSession();
+            Query query = session.createSQLQuery(sql);
+            query.setFirstResult(0);
+            query.setMaxResults(200);
+            List<String> rsaList = (List<String>) query.list();
+            return (rsaList != null && rsaList.size() > 0 ) ? rsaList : null;
+	 }
+	 
 }
