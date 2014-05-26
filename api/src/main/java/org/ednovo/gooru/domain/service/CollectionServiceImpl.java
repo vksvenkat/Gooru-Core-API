4bcadcb2644134d4a4ac97ba8c3ff2799af7a58d
charles.k <charles.kurusev@goorulearning.org>
2014-05-26 19:03:35 +0530

    added rating fields in getlibrary api

--- api/src/main/java/org/ednovo/gooru/domain/service/CollectionServiceImpl.java
+++ api/src/main/java/org/ednovo/gooru/domain/service/CollectionServiceImpl.java
@@ -327,6 +327,7 @@ public class CollectionServiceImpl extends ScollectionServiceImpl implements Col
 				}
 				item.put(SHARING, object[7]);
 				item.put(COLLECTION_ITEM_ID, object[8]);
+				item.put("ratings", this.getFeedbackService().getContentFeedbackStarRating(String.valueOf(object[1])));
 				if (!fetchChildItem) {
 					if (String.valueOf(object[2]).equalsIgnoreCase("assessment-question")) {
 						item.put("depthOfKnowledges", this.setContentMetaAssociation(this.getContentMetaAssociation("depth_of_knowledge"), String.valueOf(object[1]), "depth_of_knowledge"));
