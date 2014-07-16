a0e375ebc1c14d34273dade5317d441eac07dc57
charles.k <charles.kurusev@goorulearning.org>
2014-07-16 19:52:21 +0530

    [User & Profile] Added expiring time for cache

--- api/src/main/java/org/ednovo/gooru/domain/service/redis/RedisServiceImpl.java
+++ api/src/main/java/org/ednovo/gooru/domain/service/redis/RedisServiceImpl.java
@@ -322,8 +322,9 @@ public class RedisServiceImpl implements RedisService, ParameterProperties, Cons
 		Date currentDate = new Date();
 		long expiresTimes = 24;
 		if (organization != null) {
-			if (organizationSettingRepository.getOrganizationSetting(SESSION_EXPIRES_TIME, organization.getPartyUid()) != null) {
-				expiresTimes = Long.parseLong(organizationSettingRepository.getOrganizationSetting(SESSION_EXPIRES_TIME, organization.getPartyUid()));
+			Map<String, String> expireTimeList = settingService.getOrganizationExpireTime(SESSION_EXPIRES_TIME);
+			if (expireTimeList.containsKey(organization.getPartyUid())) {
+				expiresTimes = Long.parseLong(expireTimeList.get(organization.getPartyUid()));
 			}
 		}
 		Long createdTime = currentDate.getTime() + (1000 * 60 * 60 * expiresTimes);
