c60c9d81734d9e364ab15f2d7ff2d82ae05f949c
sheebans <sheeban.s@goorulearning.org>
2014-08-18 13:36:40 +0530

    added fix for null pointer expection

--- api/src/main/java/org/ednovo/gooru/domain/service/ScollectionServiceImpl.java
+++ api/src/main/java/org/ednovo/gooru/domain/service/ScollectionServiceImpl.java
@@ -1033,8 +1033,8 @@ public class ScollectionServiceImpl extends BaseServiceImpl implements Scollecti
 
 	@Override
 	public List<ContentMetaDTO> setContentMetaAssociation(List<ContentMetaDTO> depthOfKnowledges, String collectionId, final String type) {
-		Resource resource = this.getResourceRepository().findResourceByContentGooruId(collectionId);
-		rejectIfNull(resource, "GL0056", COLLECTION + " ID: " + collectionId);
+		Resource resource = this.getResourceRepository().findResourceByContent(collectionId);
+		rejectIfNull(resource, "GL0056", RESOURCE + " ID: " + collectionId);
 		return   setContentMetaAssociation( depthOfKnowledges,  resource.getContentMetaAssoc(),  type);
 	}
 	
