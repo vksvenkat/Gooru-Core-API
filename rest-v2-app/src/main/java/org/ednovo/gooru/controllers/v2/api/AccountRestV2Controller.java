4a947d37ab45fd68b75783c84d5da87d1ceb1b4c
venkatkumar <venkatkumar.s@goorulearning.org>
2014-08-25 17:31:10 +0530

    Removed unused cookies

--- rest-v2-app/src/main/java/org/ednovo/gooru/controllers/v2/api/AccountRestV2Controller.java
+++ rest-v2-app/src/main/java/org/ednovo/gooru/controllers/v2/api/AccountRestV2Controller.java
@@ -120,8 +120,6 @@ public class AccountRestV2Controller extends BaseController implements ConstantP
 		getAccountService().logOut(sessionToken);
 		request.getSession().invalidate();
 		RequestUtil.deleteCookie(request, response, GOORU_SESSION_TOKEN);
-		RequestUtil.deleteCookie(request, response, COOKIE_KEY_ACCESS_TOKEN);
-		RequestUtil.deleteCookie(request, response, COOKIE_KEY_REFRESH_TOKEN);
 		RequestUtil.deleteCookie(request, response, COOKIE_KEY_SERVICE_VERSION);
 		
 	}
