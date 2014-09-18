4d5899e7a4ba07db68f15d02df4ecfa4c13d1445
charles.k <charles.kurusev@goorulearning.org>
2014-09-18 15:38:29 +0530

    Added fix for 500 issue  in getUser

--- api/src/main/java/org/ednovo/gooru/domain/service/userManagement/UserManagementServiceImpl.java
+++ api/src/main/java/org/ednovo/gooru/domain/service/userManagement/UserManagementServiceImpl.java
@@ -217,7 +217,7 @@ public class UserManagementServiceImpl extends BaseServiceImpl implements UserMa
 	public Profile getUserProfile(String gooruUid, Integer activeFlag) {
 
 		User user = this.findByGooruId(gooruUid);
-		if (user == null) {
+		if (user == null || user.getGooruUId().equalsIgnoreCase(ANONYMOUS)) {
 			throw new BadRequestException(generateErrorMessage(GL0056, USER));
 		}
 		Profile profile = this.getUserRepository().getProfile(user, false);
