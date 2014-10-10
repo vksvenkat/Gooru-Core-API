52772596091ae93dbde260a157683f6b14e862e7
Sheeban <sheeban.s@goorulearning.org>
2014-10-10 19:39:19 +0530

    Impelemented application API support

---
+++ gooru-core/src/main/java/org/ednovo/gooru/core/api/model/Application.java
@@ -0,0 +1,64 @@
+
+package org.ednovo.gooru.core.api.model;
+
+public class Application extends Resource {
+
+	private static final long serialVersionUID = -2886634467039659836L;
+
+	private String key;
+	
+	private Integer searchLimit;
+	
+	private Integer limit;
+
+	private String secretKey;
+			
+	private CustomTableValue status;
+	
+	private String comment;
+
+	public String getKey() {
+		return getGooruOid();
+	}
+
+	public Integer getSearchLimit() {
+		return searchLimit;
+	}
+
+	public void setSearchLimit(Integer searchLimit) {
+		this.searchLimit = searchLimit;
+	}
+
+	public String getSecretKey() {
+		return secretKey;
+	}
+
+	public void setSecretKey(String secretKey) {
+		this.secretKey = secretKey;
+	}
+
+	public String getComment() {
+		return comment;
+	}
+
+	public void setComment(String comment) {
+		this.comment = comment;
+	}
+
+	public CustomTableValue getStatus() {
+		return status;
+	}
+
+	public void setStatus(CustomTableValue status) {
+		this.status = status;
+	}
+
+	public Integer getLimit() {
+		return limit;
+	}
+
+	public void setLimit(Integer limit) {
+		this.limit = limit;
+	}
+
+}
