f4adb664daac67b1d1a3c41f2917532222c0defc
yamunap <yamuna.p@bambeeq.com>
2014-10-29 18:15:16 +0530

    Fixed 500 error for collaboratorSuggest

--- api/src/main/java/org/ednovo/gooru/infrastructure/persistence/hibernate/collaborator/CollaboratorRepositoryHibernate.java
+++ api/src/main/java/org/ednovo/gooru/infrastructure/persistence/hibernate/collaborator/CollaboratorRepositoryHibernate.java
@@ -47,7 +47,7 @@ public class CollaboratorRepositoryHibernate extends BaseRepositoryHibernate imp
 
 	@Override
 	public List<String> collaboratorSuggest(String text, String gooruUid) {
-		String hql= "select external_id as mailId from  identity i inner join user_content_assoc uc on uc.user_uid = i.user_uid where uc.associated_by_uid=:gooruUid and i.external_id like '" +text+ "%'";
+		String hql= "select external_id as mailId from  identity i inner join user_content_assoc uc on uc.user_uid = i.user_uid where uc.associated_by_uid=:gooruUid and i.external_id like '" +text.replace("'", "\\")+ "%'";
 		Query query = getSession().createSQLQuery(hql).addScalar("mailId",StandardBasicTypes.STRING);
 		query.setParameter("gooruUid", gooruUid);
 		return query.list();
