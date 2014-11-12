6cbab9b2ba80f98283fc3c053063a762fb6c138e
saravanab <saravana.b@goorulearning.org>
2014-11-12 15:34:44 +0530

    Implemented CORE-2011(Required "CollectionItemId" while creating Folder/Collection).

--- api/src/main/java/org/ednovo/gooru/domain/service/ScollectionServiceImpl.java
+++ api/src/main/java/org/ednovo/gooru/domain/service/ScollectionServiceImpl.java
@@ -317,6 +317,7 @@ public class ScollectionServiceImpl extends BaseServiceImpl implements Scollecti
 				CollectionItem collectionItem = new CollectionItem();
 				collectionItem.setItemType(ShelfType.AddedType.ADDED.getAddedType());
 				collection.setCollectionItem(this.createCollectionItem(collection.getGooruOid(), null, collectionItem, collection.getUser(), CollectionType.SHElf.getCollectionType(), false).getModel());
+				collection.setCollectionItemId(collection.getCollectionItem().getCollectionItemId());
 			}
 			if (collection.getResourceType().getName().equalsIgnoreCase(SCOLLECTION)) {
 				if (collection.getDepthOfKnowledges() != null && collection.getDepthOfKnowledges().size() > 0) {
@@ -356,9 +357,10 @@ public class ScollectionServiceImpl extends BaseServiceImpl implements Scollecti
 			final Collection parentCollection = collectionRepository.getCollectionByGooruOid(parentId, collection.getUser().getGooruUId());
 			if (parentCollection != null) {
 				collection.setCollectionItem(this.createCollectionItem(collection.getGooruOid(), parentCollection.getGooruOid(), new CollectionItem(), collection.getUser(), CollectionType.FOLDER.getCollectionType(), false).getModel());
+				collection.setCollectionItemId(collection.getCollectionItem().getCollectionItemId());
 				getAsyncExecutor().deleteFromCache(V2_ORGANIZE_DATA + parentCollection.getUser().getPartyUid() + "*");
 			}
-
+			
 			try {
 				indexProcessor.index(collection.getGooruOid(), IndexProcessor.INDEX, SCOLLECTION);
 			} catch (Exception ex) {
