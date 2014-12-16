ea7a90d49bad1cc5b59a7e2f17973427e170fafa
saravanab <saravana.b@goorulearning.org>
2014-12-16 16:07:02 +0530

    Fixed CORE-2173([Update Resource] Missing fields in resource update)

--- rest-v2-app/src/main/java/org/ednovo/gooru/controllers/v2/api/CollectionRestV2Controller.java
+++ rest-v2-app/src/main/java/org/ednovo/gooru/controllers/v2/api/CollectionRestV2Controller.java
@@ -477,21 +477,7 @@ public class CollectionRestV2Controller extends BaseController implements Consta
 	}
 
 	private Resource buildResourceFromInputParameters(String data, User user) {
-		Resource resource = JsonDeserializer.deserialize(data, Resource.class);
-		resource.setGooruOid(UUID.randomUUID().toString());
-		ContentType contentType = getResourceService().getContentType(ContentType.RESOURCE);
-		resource.setContentType(contentType);
-		resource.setLastModified(new Date(System.currentTimeMillis()));
-		resource.setCreatedOn(new Date(System.currentTimeMillis()));
-		resource.setUser(user);
-		resource.setOrganization(user.getPrimaryOrganization());
-		resource.setCreator(user);
-		resource.setDistinguish(Short.valueOf("0"));
-		resource.setRecordSource(NOT_ADDED);
-		resource.setIsFeatured(0);
-		resource.setLastUpdatedUserUid(user.getGooruUId());
-
-		return resource;
+		return  JsonDeserializer.deserialize(data, Resource.class);
 	}
 
 	private Collection buildCopyCollectionFromInputParameters(String data) {
