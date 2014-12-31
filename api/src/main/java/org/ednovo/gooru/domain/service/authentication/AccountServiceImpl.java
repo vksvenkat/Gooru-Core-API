65c23a7fa5364cd48ea6b44ec191a279324dbea4
Purnima <purnima@goorulearning.org>
2014-12-31 15:23:39 +0530

    Core-2182 more changes in event support for gmail user login

--- api/src/main/java/org/ednovo/gooru/domain/service/authentication/AccountServiceImpl.java
+++ api/src/main/java/org/ednovo/gooru/domain/service/authentication/AccountServiceImpl.java
@@ -380,7 +380,7 @@ public class AccountServiceImpl extends ServerValidationUtils implements Account
 		if (secretKey == null || !secretKey.equalsIgnoreCase(settingService.getConfigSetting(ConfigConstants.GOORU_AUTHENTICATION_SECERT_KEY, 0, TaxonomyUtil.GOORU_ORG_UID))) {
 			throw new UnauthorizedException(generateErrorMessage("GL0082", "secret") + secretKey);
 		}
-		Boolean registerUser = false;
+		boolean registerUser = false;
 		final Identity identity = new Identity();
 		identity.setExternalId(newUser.getEmailId());
 		User userIdentity = this.getUserService().findByIdentity(identity);
