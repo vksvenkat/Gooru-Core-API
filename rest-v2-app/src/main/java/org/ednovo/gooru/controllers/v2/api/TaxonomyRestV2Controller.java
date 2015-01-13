8bd9db49e5f3c84481b001506871e054b524d3fb
prakash <prakash.m@bambeeq.com>
2015-01-13 12:48:32 +0530

    resolved changes

--- rest-v2-app/src/main/java/org/ednovo/gooru/controllers/v2/api/TaxonomyRestV2Controller.java
+++ rest-v2-app/src/main/java/org/ednovo/gooru/controllers/v2/api/TaxonomyRestV2Controller.java
@@ -97,7 +97,7 @@ public class TaxonomyRestV2Controller extends BaseController implements Constant
 			String organizationUid = user.getOrganization().getPartyUid();
 			codeId = TaxonomyUtil.getTaxonomyRootId(organizationUid);
 		}
-		if (libraryCodeList != null) {
+		if (libraryCodeList == null) {
 			libraryCodeList = serializeToJsonWithExcludes(this.getTaxonomyService().getCourseBySubject(codeId, maxLessonLimit), COURSE_EXCLUDES, true, true, COURSE_INCLUDES);
 			getRedisService().putValue(cacheKey, libraryCodeList, RedisService.DEFAULT_PROFILE_EXP);
 		}
