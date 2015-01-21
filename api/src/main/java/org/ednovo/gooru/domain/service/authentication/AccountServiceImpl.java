229620ca8ebed553c22f9713ebfe98b7fa31a0a2
Purnima <purnima@goorulearning.org>
2015-01-21 15:15:44 +0530

    Removed unwanted imports

--- api/src/main/java/org/ednovo/gooru/domain/service/authentication/AccountServiceImpl.java
+++ api/src/main/java/org/ednovo/gooru/domain/service/authentication/AccountServiceImpl.java
@@ -38,7 +38,6 @@ import org.ednovo.gooru.core.api.model.Identity;
 import org.ednovo.gooru.core.api.model.Organization;
 import org.ednovo.gooru.core.api.model.PartyCustomField;
 import org.ednovo.gooru.core.api.model.Profile;
-import org.ednovo.gooru.core.api.model.SessionContextSupport;
 import org.ednovo.gooru.core.api.model.User;
 import org.ednovo.gooru.core.api.model.UserAccountType;
 import org.ednovo.gooru.core.api.model.UserToken;
@@ -67,15 +66,12 @@ import org.ednovo.gooru.infrastructure.persistence.hibernate.apikey.ApplicationR
 import org.ednovo.gooru.infrastructure.persistence.hibernate.customTable.CustomTableRepository;
 import org.ednovo.goorucore.application.serializer.ExcludeNullTransformer;
 import org.json.JSONException;
-import org.json.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.validation.BindException;
 import org.springframework.validation.Errors;
-
-import com.google.code.javascribd.type.ApiKey;
 
 import flexjson.JSONSerializer;
 
