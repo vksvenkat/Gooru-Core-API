e5c2b20ac2496c030f47228229102b4885c4e91f
saravanab <saravana.b@goorulearning.org>
2015-02-16 13:11:37 +0530

    Removed unwanted white spaces

--- api/src/main/java/org/ednovo/gooru/infrastructure/messenger/IndexProcessor.java
+++ api/src/main/java/org/ednovo/gooru/infrastructure/messenger/IndexProcessor.java
@@ -65,7 +65,6 @@ public class IndexProcessor extends BaseComponent {
 	@Autowired
 	private RedisService redisService;
 	
-
 	private static final JSONSerializer SERIALIZER = new JSONSerializer();
 
 	
