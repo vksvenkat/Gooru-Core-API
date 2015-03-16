ed44e2114dd011bd3e4092bc2450549de256f996
viji manivanna <viji.m@bambeeq.com>
2015-03-16 12:51:58 +0530

    aligned the text.

--- gooru-core/src/main/java/org/ednovo/gooru/core/application/util/BaseUtil.java
+++ gooru-core/src/main/java/org/ednovo/gooru/core/application/util/BaseUtil.java
@@ -225,24 +225,20 @@ public class BaseUtil {
 	
     public static String md5(String password) throws Exception {
         MessageDigest md = MessageDigest.getInstance("MD5");
-       md.update(password.getBytes());
-
-       byte byteData[] = md.digest();
-
-       //convert the byte to hex format method 1
-       StringBuffer sb = new StringBuffer();
-       for (int i = 0; i < byteData.length; i++) {
-        sb.append(Integer.toString((byteData[i] & 0xff) + 0x100, 16).substring(1));
-       }
-
-       StringBuffer hexString = new StringBuffer();
-   	for (int i=0;i<byteData.length;i++) {
-   		String hex=Integer.toHexString(0xff & byteData[i]);
-  	     	if(hex.length()==1) hexString.append('0');
-  	     	hexString.append(hex);
-   	}
-   	
-   	return hexString.toString();
-   }
+        md.update(password.getBytes());
+       	byte byteData[] = md.digest();
+       	StringBuffer sb = new StringBuffer();
+	    for (int i = 0; i < byteData.length; i++) {
+	        sb.append(Integer.toString((byteData[i] & 0xff) + 0x100, 16).substring(1));
+	     }
+	    StringBuffer hexString = new StringBuffer();
+	    for (int i=0;i<byteData.length;i++) {
+	   		 String hex=Integer.toHexString(0xff & byteData[i]);
+	  	     if(hex.length()==1) hexString.append('0');
+	  	     hexString.append(hex);
+	   	 }
+	    
+	   	 return hexString.toString();
+    }
    
 }
