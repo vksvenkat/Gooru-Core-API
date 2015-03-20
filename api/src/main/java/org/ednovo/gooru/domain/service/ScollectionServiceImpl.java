7dcef8439b9dbba822eed11da9b2d4b9e27528e9
saravanab <saravana.b@goorulearning.org>
2015-03-20 18:51:02 +0530

    Changed the Sharing logic

--- api/src/main/java/org/ednovo/gooru/domain/service/ScollectionServiceImpl.java
+++ api/src/main/java/org/ednovo/gooru/domain/service/ScollectionServiceImpl.java
@@ -2134,7 +2134,7 @@ public class ScollectionServiceImpl extends BaseServiceImpl implements Scollecti
 			if (newCollection != null && newCollection.getSharing() != null) {
 				destCollection.setSharing(newCollection.getSharing());
 			} else {
-			      destCollection.setSharing(addToShelf ? Sharing.ANYONEWITHLINK.getSharing() : sourceCollection.getSharing().equalsIgnoreCase(Sharing.PUBLIC.getSharing()) ? Sharing.ANYONEWITHLINK.getSharing() :  sourceCollection.getSharing());
+			      destCollection.setSharing(sourceCollection.getSharing().equalsIgnoreCase(Sharing.PUBLIC.getSharing())?Sharing.ANYONEWITHLINK.getSharing(): sourceCollection.getSharing());
 			}
 			destCollection.setUser(user);
 			destCollection.setOrganization(sourceCollection.getOrganization());
