6ca0caef4e98a829af821383c53002b2bbfdee2e
viji-m <viji.m@bambeeq.com>
2015-05-05 20:15:54 +0530

    changed the logic for reading the CSV file.

--- api/src/main/java/org/ednovo/gooru/domain/service/user/UserImportServiceImpl.java
+++ api/src/main/java/org/ednovo/gooru/domain/service/user/UserImportServiceImpl.java
@@ -1,8 +1,6 @@
 package org.ednovo.gooru.domain.service.user;
 
-import java.io.BufferedReader;
 import java.io.FileReader;
-import java.io.IOException;
 import java.util.ArrayList;
 
 import javax.servlet.http.HttpServletRequest;
@@ -16,6 +14,8 @@ import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import au.com.bytecode.opencsv.CSVReader;
+
 @Service
 public class UserImportServiceImpl extends FileImporter implements UserImportService {
 
@@ -25,42 +25,35 @@ public class UserImportServiceImpl extends FileImporter implements UserImportSer
 	@Override
 	public void createUser(String filename, User apiCaller, HttpServletRequest request) {
 		final String mediaFileName = UserGroupSupport.getUserOrganizationNfsInternalPath() + Constants.UPLOADED_MEDIA_FOLDER +'/'+ filename;
-		BufferedReader br = null;
-		String line = "";
-		String cvsSplitBy = "#";
-		int first = 0;
 		ArrayList<String> header = new ArrayList<String>();
+		int first = 0;
+		String json = "";
+		CSVReader csvReader;
 		try {
-			br = new BufferedReader(new FileReader(mediaFileName));
-			while ((line = br.readLine()) != null) {
-				String json = "{";
-				String[] userDetails = line.split(cvsSplitBy);
-				for (int i = 0; i < userDetails.length; i++) {
-					if (first == 0) {
-						header.add('"' + userDetails[i] + "\":\"");
-					} else {
-						json += header.get(i).toString() + userDetails[i] + "\",";
-					}
-				}
-				json = json.substring(0, json.length() - 1) + '}';
+			csvReader = new CSVReader(new FileReader(mediaFileName));
+			String[] row = null;
+			while((row = csvReader.readNext()) != null) {
+			    for(int i=0; i<row.length; i++){
+			    	if(first==0){
+			    		header.add(row[i]);
+			    	}
+			    	else{
+			    		json += "\"" + header.get(i) + "\":\"" + row[i] + "\",";
+			    	}
+			    }
 				if (first == 1) {
+				    json = json.substring(0, json.length() - 1) + '}';
+				    System.out.print(json);
 					JSONObject jsonObj = requestData(generateJSONInput(json, UNDER_SCORE));
 					final User user = this.buildUserFromInputParameters((getValue(USER, jsonObj)));
 					this.getUserManagementService().createUserWithValidation(user, jsonObj.get(PASSWORD).toString(), null, null, false, false, apiCaller, null, jsonObj.get(DATEOFBIRTH).toString(), null, jsonObj.get(GENDER).toString(), null, null, json, false, request, null, null);
 				}
 				first = 1;
+				json = "{";
 			}
 
 		} catch (Exception e) {
 			e.printStackTrace();
-		} finally {
-			if (br != null) {
-				try {
-					br.close();
-				} catch (IOException e) {
-					e.printStackTrace();
-				}
-			}
 		}
 
 	}
