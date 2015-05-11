6a538449d0a12c852b90dab7ca151a0357c2d829
viji-m <viji.m@bambeeq.com>
2015-05-11 20:13:57 +0530

    added the copy rights for newly created java files.

--- api/src/main/java/org/ednovo/gooru/domain/service/party/OrganizationImportServiceImpl.java
+++ api/src/main/java/org/ednovo/gooru/domain/service/party/OrganizationImportServiceImpl.java
@@ -1,3 +1,26 @@
+/////////////////////////////////////////////////////////////
+// OrganizationImportServiceImpl.java
+// gooru-api
+// Created by Gooru on 2014
+// Copyright (c) 2014 Gooru. All rights reserved.
+// http://www.goorulearning.org/
+// Permission is hereby granted, free of charge, to any person obtaining
+// a copy of this software and associated documentation files (the
+// "Software"), to deal in the Software without restriction, including
+// without limitation the rights to use, copy, modify, merge, publish,
+// distribute, sublicense, and/or sell copies of the Software, and to
+// permit persons to whom the Software is furnished to do so, subject to
+// the following conditions:
+// The above copyright notice and this permission notice shall be
+// included in all copies or substantial portions of the Software.
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+// LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+// OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+// WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+/////////////////////////////////////////////////////////////
 package org.ednovo.gooru.domain.service.party;
 
 import java.io.File;
@@ -31,7 +54,7 @@ public class OrganizationImportServiceImpl extends FileImporter implements Organ
 	private OrganizationService organizationService;
 
 	@Override
-	public void createOrganization(String filename, HttpServletRequest request) throws Exception {
+	public void createOrganization(String filename, HttpServletRequest request){
 		final String mediaFileName = UserGroupSupport.getUserOrganizationNfsInternalPath() + Constants.UPLOADED_MEDIA_FOLDER + '/' + filename;
 		List<String> keys = null;
 		StringBuffer json = new StringBuffer();
