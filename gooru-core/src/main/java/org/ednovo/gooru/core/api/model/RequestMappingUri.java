12a456ef903d265abfe3229ce76853bd05a770d4
yamunap <yamuna.p@bambeeq.com>
2015-06-18 19:11:35 +0530

    Implemented MetaRestController for get meta values moved MetaRestController into v1 package

--- gooru-core/src/main/java/org/ednovo/gooru/core/api/model/RequestMappingUri.java
+++ gooru-core/src/main/java/org/ednovo/gooru/core/api/model/RequestMappingUri.java
@@ -10,7 +10,7 @@ public class RequestMappingUri {
 	
 	public static final String DOMAIN = "/v1/domain";
 	
-	public static final String V3_CLASS = "/v1/v3/class";
+	public static final String V3_CLASS = "/v3/class";
 	
 	public static final String SEPARATOR = "/";
 	
